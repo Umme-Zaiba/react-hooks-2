de082bc8090fae32f62a37e9758b68ccdc433aed
Umme-Zaiba <ummezaiba24j@gmail.com>
2024-06-07 23:16:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // import Counter from "./components/useState/Counter";
 import './App.css'
-import ObjectState from './components/useState/ObjectState';
+// import ObjectState from './components/useState/ObjectState';
 // import Prevstate from "./components/useState/Prevstate";
-
+// import Array from './components/useState/Array';
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
       {/* <Prevstate/> */}
-      <ObjectState/>
+      {/* <ObjectState/> */}
+    {/* <Array/> */}
     </div>
   );
 }
---
+++ src/components/useEffect/HookCounter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HookCounter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookCounter
