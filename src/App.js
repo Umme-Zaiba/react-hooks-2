769c58081abd3e64595ff9154d0654f31f2e514e
Umme-Zaiba <ummezaiba24j@gmail.com>
2024-06-07 23:25:19 +0530

    update useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 // import Counter from "./components/useState/Counter";
 import './App.css'
+import HookCounter from './components/useEffect/HookCounter';
 // import ObjectState from './components/useState/ObjectState';
 // import Prevstate from "./components/useState/Prevstate";
 // import Array from './components/useState/Array';
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
-      {/* <Prevstate/> */}
-      {/* <ObjectState/> */}
-    {/* <Array/> */}
+      {/* USESTATE */}
+        {/* <Counter/> */}
+        {/* <Prevstate/> */}
+        {/* <ObjectState/> */}
+        {/* <Array/> */}
+
+      {/* USEFFECT */}
+      <HookCounter/>
     </div>
   );
 }
--- src/components/useEffect/HookCounter.js
+++ src/components/useEffect/HookCounter.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const HookCounter = () => {
+    const [count, setCount]=useState(0);
+
+    useEffect( ()=>{
+        document.title = `You clicked ${count} times`
+    })
+    //useEffect runs after every time the component renders 
   return (
     <div>
-      
+      <button onClick={()=> setCount(count +1)}>Click {count} times</button>
     </div>
   )
 }
